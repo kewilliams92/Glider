d437af8ec3fe8f024591184bb702392eaf670f5e
kw92 <kewilliams2017@hotmail.com>
2023-04-12 13:23:00 -0400

    route grouping

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	
 	import TiImageOutline from 'svelte-icons/ti/TiImageOutline.svelte';
-	import GlidePost from '../components/glides/GlidePost.svelte';
+	import GlidePost from '../../components/glides/GlidePost.svelte';
 
 	let glides = []
 	let glideContent = ""
