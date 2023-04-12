006478864b96329eec44021de34af4143b3c785c
kw92 <kewilliams2017@hotmail.com>
2023-04-12 13:49:51 -0400

    finished error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {page} from "$app/stores";
+</script>
+
+<div class='text-white flex-it justify-center items-center h-full'>
+<div class='text-center'>
+    <div class='text-4xl font-bold'>
+        {$page.status} - {$page.error.message}
+    </div>
+    <a href='/' class='block text-blue-400 hover:underline text-center'>
+        Go to Homepage
+    </a>
+</div>
+</div>
